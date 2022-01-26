9fa4b12a8350df10d61ba3cdda8995f60f6beede
RINAT SULTANOV <r.sultanov@realweb.ru>
2022-01-27 00:25:38 +0300

    fb temporary stopped

--- models/staging/stg_facebook_cab.sql
+++ models/staging/stg_facebook_cab.sql
@@ -1,88 +1,88 @@
-{% if target.name == 'prod' %}
+-- {% if target.name == 'prod' %}
 
   
-{{
-  config(
-    materialized = 'incremental',
-    partition_by = {
-      "field": "date",
-      "data_type": "date",
-      "granularity": "day"
-    },
-    cluster_by = ["campaign_name"],
-  )
-}}
+-- {{
+--   config(
+--     materialized = 'incremental',
+--     partition_by = {
+--       "field": "date",
+--       "data_type": "date",
+--       "granularity": "day"
+--     },
+--     cluster_by = ["campaign_name"],
+--   )
+-- }}
 
-{% endif %}
+-- {% endif %}
 
-{% if not is_incremental() %}
+-- {% if not is_incremental() %}
 
-    SELECT DISTINCT
-        date,
-        campaign_name,
-        adset_name,
-        ad_name,
-        show,
-        clicks,
-        installs,
-        spend,
-        cnt_installs,
-        cnt_af_purchase,
-        revenue,
-        cnt_first_purchase,
-        first_purchase_revenue,
-        add_to_card
-    FROM {{ source('sheets_data', 'FBNEW_data') }}
+--     SELECT DISTINCT
+--         date,
+--         campaign_name,
+--         adset_name,
+--         ad_name,
+--         show,
+--         clicks,
+--         installs,
+--         spend,
+--         cnt_installs,
+--         cnt_af_purchase,
+--         revenue,
+--         cnt_first_purchase,
+--         first_purchase_revenue,
+--         add_to_card
+--     FROM {{ source('sheets_data', 'FBNEW_data') }}
 
-    UNION ALL
+--     UNION ALL
 
-{% endif %}
+-- {% endif %}
 
--- SELECT DISTINCT
+-- -- SELECT DISTINCT
+-- --     date,
+-- --     campaign_name,
+-- --     adset_name,
+-- --     ad_name,
+-- --     show,
+-- --     clicks,
+-- --     installs,
+-- --     spend,
+-- --     cnt_installs,
+-- --     cnt_af_purchase,
+-- --     revenue,
+-- --     cnt_first_purchase,
+-- --     first_purchase_revenue,
+-- --     add_to_card
+-- -- FROM {{ this }}
+
+-- -- UNION ALL 
+
+-- SELECT 
 --     date,
---     campaign_name,
---     adset_name,
---     ad_name,
---     show,
---     clicks,
---     installs,
---     spend,
---     cnt_installs,
---     cnt_af_purchase,
---     revenue,
---     cnt_first_purchase,
---     first_purchase_revenue,
---     add_to_card
--- FROM {{ this }}
+--     campaign as campaign_name,
+--     adset as adset_name,
+--     ad as ad_name,
+--     SUM(IFNULL(show,0)) as show,
+--     SUM(IFNULL(clicks,0)) as clicks,
+--     SUM(IFNULL(installs,0)) as installs,
+--     SUM(IFNULL(spend,0)) as spend,
+--     SUM(IFNULL(installs,0)) as cnt_installs,
+--     SUM(IFNULL(purchase,0)) as cnt_af_purchase,
+--     SUM(IFNULL(purchase_revenue,0)) as revenue,
+--     SUM(IFNULL(first_purchase,0)) as cnt_first_purchase,
+--     SUM(IFNULL(first_purchase_revenue,0)) as first_purchase_revenue,
+--     SUM(IFNULL(ad_to_cars,0)) as add_to_card,
+-- FROM {{ source('sheets_data', 'fb_spreadsheets_data') }}
+-- WHERE date IS NOT NULL
 
--- UNION ALL 
+-- {% if is_incremental() %}
 
-SELECT 
-    date,
-    campaign as campaign_name,
-    adset as adset_name,
-    ad as ad_name,
-    SUM(IFNULL(show,0)) as show,
-    SUM(IFNULL(clicks,0)) as clicks,
-    SUM(IFNULL(installs,0)) as installs,
-    SUM(IFNULL(spend,0)) as spend,
-    SUM(IFNULL(installs,0)) as cnt_installs,
-    SUM(IFNULL(purchase,0)) as cnt_af_purchase,
-    SUM(IFNULL(purchase_revenue,0)) as revenue,
-    SUM(IFNULL(first_purchase,0)) as cnt_first_purchase,
-    SUM(IFNULL(first_purchase_revenue,0)) as first_purchase_revenue,
-    SUM(IFNULL(ad_to_cars,0)) as add_to_card,
-FROM {{ source('sheets_data', 'fb_spreadsheets_data') }}
-WHERE date IS NOT NULL
+--     AND ARRAY_TO_STRING([campaign,adset,ad,CAST(date AS STRING)],'') NOT IN 
+--     (
+--         SELECT DISTINCT ARRAY_TO_STRING([campaign,adset,ad,CAST(date AS STRING)],'') 
+--         FROM {{ this }}
+--     )
 
-{% if is_incremental() %}
+-- {% endif %}
 
-    AND ARRAY_TO_STRING([campaign,adset,ad,CAST(date AS STRING)],'') NOT IN 
-    (
-        SELECT DISTINCT ARRAY_TO_STRING([campaign,adset,ad,CAST(date AS STRING)],'') 
-        FROM {{ this }}
-    )
-
-{% endif %}
-
-GROUP BY 1,2,3,4
+-- GROUP BY 1,2,3,4
