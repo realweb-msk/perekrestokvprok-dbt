d8e6dde845238273c2953de067a0ef1af48385c3
RINAT SULTANOV <r.sultanov@realweb.ru>
2022-02-02 15:06:37 +0300

    some marts added

---
+++ models/marts/dim_client_promo.sql
@@ -0,0 +1,36 @@
+---костов здесь нет, добавь в PBI---
+WITH promo_client_data AS (
+    SELECT
+        order_date,
+        region,
+        promo_name,
+        promo_name_full,
+        LOWER(promo_code) AS promo_code,
+        order_id,
+        platform,
+        revenue,
+        order_count
+    FROM {{ ref('stg_promocode_client_data') }}
+),
+
+promo_dict AS (
+    SELECT
+        LOWER(promocode) AS promo_code,
+        type,
+        channel
+    FROM {{ ref('stg_promo_sheets') }}
+)
+
+SELECT
+    DATE(order_date) AS date,
+    channel,
+    type,
+    UPPER(promo_code) AS promo_code,
+    promo_name,
+    region,
+    revenue,
+    order_count
+FROM promo_client_data
+LEFT JOIN promo_dict
+USING(promo_code)
+WHERE type IS NOT NULL
--- models/staging/int_google_dbm_impressions_clicks_revenue_meta.sql
+++ models/staging/int_google_dbm_impressions_clicks_revenue_meta.sql
@@ -15,11 +15,11 @@ WITH old_source AS (
 old_modified AS (
     SELECT
         date,
-        insertionOrder,
-        insertionOrderID,
+        insertionOrder AS insertion_order,
+        insertionOrderID AS insertion_order_id,
         SUM(SAFE_CAST(impressions AS INT64)) AS impressions,
         SUM(SAFE_CAST(clicks AS INT64)) AS clicks,
-        SUM(SAFE_CAST(revenueAdvCurrency AS FLOAT64)) AS revenueAdvCurrency
+        SUM(SAFE_CAST(revenueAdvCurrency AS FLOAT64)) AS revenue_adv_currency
     FROM old_source
     WHERE pd_rw = 1
     GROUP BY 1, 2, 3
@@ -28,27 +28,24 @@ old_modified AS (
 new_source AS (
     SELECT
         date,
-        insertionOrder,
-        insertionOrderID,
+        insertion_order,
+        insertion_order_id,
         impressions,
         clicks,
-        revenueAdvCurrency,
-        ROW_NUMBER() OVER (PARTITION BY InsertionOrderID, LineItemID, Date ORDER BY DATE(_PARTITIONTIME) DESC) 
-            AS pd_rw
-    FROM {{ source('test2', 'google_dbm_existed_dbm_report_123') }}
+        revenue_adv_currency,
+    FROM {{ ref('stg_google_dbm') }}
     WHERE date >= '2021-12-01'
 ),
 
 new_modified AS (
     SELECT
         date,
-        insertionOrder,
-        insertionOrderID,
+        insertion_order,
+        insertion_order_id,
         SUM(SAFE_CAST(impressions AS INT64)) AS impressions,
         SUM(SAFE_CAST(clicks AS INT64)) AS clicks,
-        SUM(SAFE_CAST(revenueAdvCurrency AS FLOAT64)) AS revenueAdvCurrency
-    FROM old_source
-    WHERE pd_rw = 1
+        SUM(SAFE_CAST(revenue_adv_currency AS FLOAT64)) AS revenue_adv_currency
+    FROM new_source
     GROUP BY 1, 2, 3
 ),
 
@@ -62,9 +59,9 @@ final AS (
 
 SELECT
     date,
-    insertionOrder AS insertion_order,
-    insertionOrderID AS insertion_order_id,
+    insertion_order,
+    insertion_order_id,
     impressions,
     clicks,
-    revenueAdvCurrency AS revenue_adv_currency
+    revenue_adv_currency
 FROM final
