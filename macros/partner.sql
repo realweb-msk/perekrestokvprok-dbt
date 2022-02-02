96def9784b30381fa623b6733413eec13a833e18
RINAT SULTANOV <r.sultanov@realweb.ru>
2022-02-03 00:06:57 +0300

    small fix

--- macros/partner.sql
+++ macros/partner.sql
@@ -4,7 +4,7 @@
         WHEN REGEXP_CONTAINS({{campaign_name}}, r'_tl_') THEN '2leads'
         WHEN REGEXP_CONTAINS({{campaign_name}}, r'_mx_') THEN 'MobX'
         WHEN REGEXP_CONTAINS({{campaign_name}}, r'_sw_') THEN 'SW'
-        WHEN REGEXP_CONTAINS({{campaign_name}}, r'_ms_') THEN 'Think Mobile'
+        WHEN REGEXP_CONTAINS({{campaign_name}}, r'_tm_') THEN 'Think Mobile'
         WHEN REGEXP_CONTAINS({{campaign_name}}, r'_abc_|_sf_') THEN 'Mediasurfer'
     ELSE '-' END
 {% endmacro %}
