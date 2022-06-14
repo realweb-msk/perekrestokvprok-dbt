6c5960ea303d6700302a3444df178ff13f68d9e0
dlazuta <0466300@gmail.com>
2022-06-14 19:50:31 +0300

    Add: MT cost FROM VK Beta Cab

--- macros/platform.sql
+++ macros/platform.sql
@@ -1,6 +1,6 @@
 {% macro platform(campaign_name) %}
     CASE
         WHEN REGEXP_CONTAINS(LOWER({{campaign_name}}), r'\[p:ios\]|_ios_|p02') THEN 'ios'
-        WHEN REGEXP_CONTAINS(LOWER({{campaign_name}}), r'\[p:and\]|_and_|android|p01') THEN 'android'
+        WHEN REGEXP_CONTAINS(LOWER(campaign_name), r'\[p:and\]|_and_|android|p01|:and_') THEN 'android'
     ELSE 'no_platform' END
 {% endmacro %}
---
+++ models/staging/stg_vk_beta_sheet.sql
@@ -0,0 +1,49 @@
+{% if target.name == 'prod' %}
+
+{{
+  config(
+    materialized='incremental',
+    unique_key='unique_key',
+    partition_by={
+      "field": "date",
+      "data_type": "date",
+      "granularity": "day"
+    },
+    cluster_by = ["campaign_type"]
+  )
+}}
+
+{% endif %}
+
+WITH source AS (
+    SELECT
+        PARSE_DATE('%d.%m.%Y', date) AS date,
+        {{ normalize('campaign_name')}} campaign_name,
+        SUM(COALESCE(SAFE_CAST(REGEXP_REPLACE(REGEXP_REPLACE(cost, r',', '.'), r' ', '') AS FLOAT64), 0)) as cost
+    FROM perekrestokvprok-bq.sheets_data.vk_beta_sheet
+    WHERE date IS NOT NULL
+    GROUP BY date, campaign_name
+),
+
+final AS (
+    SELECT
+        ARRAY_TO_STRING([
+            CAST(date AS STRING),
+            campaign_name
+        ],'') AS unique_key,
+        date,
+        campaign_name,
+        IF(
+          REGEXP_CONTAINS(campaign_name, r'[_\[]new[\]_]|_new_install_'),'UA','retargeting'
+          ) AS campaign_type,
+        cost
+    FROM source
+)
+
+SELECT
+    unique_key,
+    date,
+    campaign_name,
+    campaign_type,
+    cost
+FROM final
